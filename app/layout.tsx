9de19949f15c30d59d693deacb0db56764730db4
zrodkin <zalmanrodkin@gmail.com>
2025-06-17 03:57:02 -0400

    added logo for google search

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export const metadata: Metadata = {
   // Favicon configuration
   icons: {
     icon: [
+      { url: '/icon-192x192.png', sizes: '192x192', type: 'image/png' },
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
       { url: '/favicon.ico' }
